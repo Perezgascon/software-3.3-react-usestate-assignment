64b55fada7f154fa36f24ab3cc4047dd7bfc2eb8
Perezgascon <76476520+Perezgascon@users.noreply.github.com>
2023-11-22 10:43:04 +0700

    init commit

---
+++ src/Components/Counter.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+
+    const [ count, setCount ] = useState(0)
+
+    const minusHandler = () => {
+        if (count > 0) { setCount(count - 1) }
+    }
+
+    const plusHandler = () => {
+        setCount(count + 1)
+    }
+
+    const resetValue = () => {
+        setCount(0);
+    }
+
+  return (
+    <div>
+        <button onClick={minusHandler}>-</button>
+        <span>{count}</span>
+        <button onClick={plusHandler}>+</button>
+        <hr />
+        <button onClick={resetValue}>RESET</button>
+    </div>
+  )
+}
